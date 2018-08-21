b5bb384b0b3ab13858f3879179b8068fd1250fce
Joseph Watts <jwatts43@bloomberg.net>
2018-08-21 15:25:52 -0400

    Accept baseline for private named function valued properties.

---
+++ tests/baselines/reference/functionValuedProperty.js
@@ -0,0 +1,34 @@
+//// [functionValuedProperty.ts]
+class A {
+    #func: (x: string) => number;
+    test() {
+        const x = this.#func("hello");
+        this.#func = function (x: string) {
+            return parseInt(x);
+        };
+        const y = (this.#func)("test");
+        (this.#func = function (x: string) {
+            return 4;
+        })("");
+    }
+}
+
+
+//// [functionValuedProperty.js]
+var _classPrivateFieldGet = function (receiver, privateMap) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return privateMap.get(receiver); };
+var _classPrivateFieldSet = function (receiver, privateMap, value) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to set private field on non-instance"); } privateMap.set(receiver, value); return value; };
+var A = /** @class */ (function () {
+    function A() {
+    }
+    A.prototype.test = function () {
+        var x = _classPrivateFieldGet(this, _func).apply(this, "hello");
+        _classPrivateFieldSet(this, _func, function (x) {
+            return parseInt(x);
+        });
+        var y = (_classPrivateFieldGet(this, _func)).apply(this, "test");
+        (_classPrivateFieldSet(this, _func, function (x) {
+            return 4;
+        }))("");
+    };
+    return A;
+}());
