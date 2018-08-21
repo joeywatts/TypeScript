7be1fd156115444a6257c657d2c110c80828728b
Joseph Watts <jwatts43@bloomberg.net>
2018-08-21 16:05:29 -0400

    Accept baseline for unary mutation of private named property

---
+++ tests/baselines/reference/propertyUnaryMutation.js
@@ -0,0 +1,42 @@
+//// [propertyUnaryMutation.ts]
+class A {
+    #property: number = 1;
+    postfixPlusPlus() {
+        this.#property++;
+    }
+    prefixPlusPlus() {
+        ++this.#property;
+    }
+    prefixMinusMinus() {
+        --this.#property;
+    }
+    postfixMinusMinus() {
+        this.#property--;
+    }
+}
+
+
+//// [propertyUnaryMutation.js]
+var _classPrivateFieldGet = function (receiver, privateMap) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return privateMap.get(receiver); };
+var _classPrivateFieldSet = function (receiver, privateMap, value) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to set private field on non-instance"); } privateMap.set(receiver, value); return value; };
+var A = /** @class */ (function () {
+    function A() {
+        _property.set(this, 1);
+    }
+    A.prototype.postfixPlusPlus = function () {
+        var _a;
+        _classPrivateFieldSet(this, _property, ((_a = _classPrivateFieldGet(this, _property)) + 1, _a));
+    };
+    A.prototype.prefixPlusPlus = function () {
+        _classPrivateFieldSet(this, _property, _classPrivateFieldGet(this, _property) + 1);
+    };
+    A.prototype.prefixMinusMinus = function () {
+        _classPrivateFieldSet(this, _property, _classPrivateFieldGet(this, _property) - 1);
+    };
+    A.prototype.postfixMinusMinus = function () {
+        var _a;
+        _classPrivateFieldSet(this, _property, ((_a = _classPrivateFieldGet(this, _property)) - 1, _a));
+    };
+    return A;
+}());
+var _property = new WeakMap;
