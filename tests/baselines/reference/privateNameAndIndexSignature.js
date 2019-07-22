4a8da9027180e0b3cf4ca2dc8fa7c0bfb09573e1
Max Heiber <max.heiber@gmail.com>
2019-07-22 14:44:23 +0100

    add test to hit symbolToExpression w private names
    symbolToExpression knows about private names
    add a test to exercise this code path

--- tests/baselines/reference/privateNameAndIndexSignature.js
+++ tests/baselines/reference/privateNameAndIndexSignature.js
@@ -3,18 +3,27 @@
 
 class A {
     [k: string]: any;
+    #foo = 3;
+    ["#bar"] = this["#bar"]   // Error (private identifiers should not prevent circularity checking for computeds)
     constructor(message: string) {
-        this.#f = 3                 // Error Property '#f' does not exist on type 'A'.
+        this.#f = 3           // Error (index signatures do not implicitly declare private names)
+        this["#foo"] = 3;     // Error: property not declared (cannot use index signature to access private name)
+
     }
 }
 
 
 //// [privateNameAndIndexSignature.js]
-"use strict";
 // @target es6
+var _foo;
+"use strict";
 var A = /** @class */ (function () {
     function A(message) {
-        this.f = 3; // Error Property '#f' does not exist on type 'A'.
+        _foo.set(this, 3);
+        this["#bar"] = this["#bar"]; // Error (private identifiers should not prevent circularity checking for computeds)
+        this.f = 3; // Error (index signatures do not implicitly declare private names)
+        this["#foo"] = 3; // Error: property not declared (cannot use index signature to access private name)
     }
     return A;
 }());
+_foo = new WeakMap();
--- tests/cases/conformance/classes/members/privateNames/privateNameAndIndexSignature.ts
+++ tests/cases/conformance/classes/members/privateNames/privateNameAndIndexSignature.ts
@@ -3,7 +3,11 @@
 
 class A {
     [k: string]: any;
+    #foo = 3;
+    ["#bar"] = this["#bar"]   // Error (private identifiers should not prevent circularity checking for computeds)
     constructor(message: string) {
-        this.#f = 3                 // Error Property '#f' does not exist on type 'A'.
+        this.#f = 3           // Error (index signatures do not implicitly declare private names)
+        this["#foo"] = 3;     // Error: property not declared (cannot use index signature to access private name)
+
     }
 }
