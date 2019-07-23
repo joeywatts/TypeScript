519863ad856f55beb3059200456961a6dd10afb9
Max Heiber <max.heiber@gmail.com>
2019-07-23 14:47:52 +0100

    Update comment in checker.ts re reserved members

--- src/compiler/checker.ts
+++ src/compiler/checker.ts
@@ -2948,7 +2948,7 @@ namespace ts {
         // A reserved member name starts with two underscores, but the third character cannot be an underscore,
         // @, or #. A third underscore indicates an escaped form of an identifier that started
         // with at least two underscores. The @ character indicates that the name is denoted by a well known ES
-        // Symbol instance and the # indicates that the name is a PrivateIdentifier.
+        // Symbol instance and the # character indicates that the name is a PrivateIdentifier.
         function isReservedMemberName(name: __String) {
             return (name as string).charCodeAt(0) === CharacterCodes._ &&
                 (name as string).charCodeAt(1) === CharacterCodes._ &&
