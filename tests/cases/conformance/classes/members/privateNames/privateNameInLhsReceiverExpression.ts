8fd22a13fbd42bc6193d9a578e4ed2cb59fc1915
Joey Watts <jwatts43@bloomberg.net>
2019-07-29 17:51:12 -0400

    Improve test case for private name assignment
    Adds a compound assignment test case for a class with private names
    being declared on the left-hand-side of the assignment expression.
    
    Signed-off-by: Joey Watts <jwatts43@bloomberg.net>

--- tests/cases/conformance/classes/members/privateNames/privateNameInLhsReceiverExpression.ts
+++ tests/cases/conformance/classes/members/privateNames/privateNameInLhsReceiverExpression.ts
@@ -3,6 +3,7 @@ class Test {
     #y = 123;
     static something(obj: { [key: string]: Test }) {
         obj[(new class { #x = 1; readonly s = "prop"; }).s].#y = 1;
+        obj[(new class { #x = 1; readonly s = "prop"; }).s].#y += 1;
     }
 }
 
